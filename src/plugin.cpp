edb202855d7537381ac01dbc5d6138f99ac76e6f
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-29 21:57:55 -0700

    Cleanup

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -2,7 +2,6 @@
 
 #include <algorithm>
 #include <cstdint>
-#include <stdexcept>
 #include <string>
 #include <unordered_map>
 
@@ -67,10 +66,7 @@ static std::string toLower(const std::string& str) {
 std::optional<std::uint32_t> getSpellIndexFromSpellName(const std::string& spellName) {
     std::string lowerSpellName = toLower(spellName);
     auto        it             = spellNameToSpellIndex.find(lowerSpellName);
-    if (it != spellNameToSpellIndex.end()) {
-        return it->second;
-    }
-    // throw std::runtime_error("Spell not found: " + spellName);
+    if (it != spellNameToSpellIndex.end()) return it->second;
     return std::nullopt;
 }
 
@@ -90,94 +86,121 @@ public:
         auto* magicEffectForm = RE::TESForm::LookupByID(event->magicEffect);
         if (!magicEffectForm) return RE::BSEventNotifyControl::kContinue;
 
-        Log("Magic Effect name: {}", magicEffectForm->GetName());
-        Log("Magic Effect ID: {}", magicEffectForm->GetFormID());
-        Log("Magic Effect type: {}", magicEffectForm->GetFormType());
-
         if (!forgottenMagicFile) return RE::BSEventNotifyControl::kContinue;
         if (!forgottenMagicFile->IsFormInMod(event->magicEffect))
             return RE::BSEventNotifyControl::kContinue;
 
-        // auto* magicEffectForm = RE::TESForm::LookupByID(event->magicEffect);
-        // if (!magicEffectForm) return RE::BSEventNotifyControl::kContinue;
-
-        Log("Magic Effect from Forgotten Magic: {}", magicEffectForm->GetName());
-
         auto* vm = RE::BSScript::Internal::VirtualMachine::GetSingleton();
         if (!vm) return RE::BSEventNotifyControl::kContinue;
-        Log("Got VM!");
 
         auto* handlePolicy = vm->GetObjectHandlePolicy();
         if (!handlePolicy) return RE::BSEventNotifyControl::kContinue;
-        Log("Got handle policy!");
 
-        auto spellIndex = getSpellIndexFromSpellName(magicEffectForm->GetName());
-        if (!spellIndex) {
-            Log("Spell index not found!");
-            return RE::BSEventNotifyControl::kContinue;
-        } else {
-            Log("Spell index found: {}", *spellIndex);
-        }
+        auto spellIndexValue = getSpellIndexFromSpellName(magicEffectForm->GetName());
+        if (!spellIndexValue) return RE::BSEventNotifyControl::kContinue;
+        auto spellIndex = *spellIndexValue;
 
         // Let's right here, right now, try to get the MCM.
         auto* mcmForm = RE::TESQuest::LookupByEditorID("vMCM");
         if (!mcmForm) return RE::BSEventNotifyControl::kContinue;
-        Log("Got MCM form!");
 
         auto mcmFormHandle = handlePolicy->GetHandleForObject(mcmForm->GetFormType(), mcmForm);
         if (!mcmFormHandle) return RE::BSEventNotifyControl::kContinue;
-        Log("Got MCM form handle!");
 
         auto mcmFormScripts = vm->attachedScripts.find(mcmFormHandle);
         if (mcmFormScripts == vm->attachedScripts.end()) return RE::BSEventNotifyControl::kContinue;
         if (mcmFormScripts != vm->attachedScripts.end()) {
             for (auto& script : mcmFormScripts->second) {
                 if (script->type->name == "vMCMscript") {
-                    Log("Found vMCM script!");
-                    // We found the vMCM script! Let's call the function to set the spell index.
+                    // We need these properties
                     auto* xpBySpellIdProperty = script->GetProperty("fSPXP");
-                    if (xpBySpellIdProperty) {
-                        auto xpBySpellIdArray = xpBySpellIdProperty->GetArray();
-                        Log("Got fSPXP property!");
+                    if (!xpBySpellIdProperty) {
+                        Log("fSPXP property not found!");
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
+                    auto* xpReqProperty = script->GetProperty("fXPreq");
+                    if (!xpReqProperty) {
+                        Log("fXPreq property not found!");
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
 
-                        auto item      = (*xpBySpellIdArray)[*spellIndex];
-                        auto currentXp = item.GetFloat();
+                    // And each property should be an array
+                    if (!xpBySpellIdProperty->IsArray()) {
+                        Log("fSPXP property is not an array!");
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
+                    if (!xpReqProperty->IsArray()) {
+                        Log("fXPreq property is not an array!");
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
 
-                        auto* xpReqProperty = script->GetProperty("fXPreq");
-                        if (xpReqProperty) {
-                            Log("Got fXPreq property!");
-                            auto xpReqsBySpellIdArray = xpReqProperty->GetArray();
+                    // Get the arrays
+                    auto xpBySpellIdArray     = xpBySpellIdProperty->GetArray();
+                    auto xpReqsBySpellIdArray = xpReqProperty->GetArray();
 
-                            auto xpReq = (*xpReqsBySpellIdArray)[*spellIndex].GetFloat();
-                            Log("XP required: {}", xpReq);
+                    // Make sure the spell index is valid in both arrays
+                    if (xpBySpellIdArray->size() <= spellIndex) {
+                        Log("XP array size is smaller than spell index!");
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
+                    if (xpReqsBySpellIdArray->size() <= spellIndex) {
+                        Log("XP req array size is smaller than spell index!");
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
 
-                            auto progress = (currentXp / xpReq) * 100;
-                            Log("Progress: {}", progress);
+                    // Get the variable at the right spell index
+                    auto currentXpVariable = (*xpBySpellIdArray)[spellIndex];
+                    auto xpReqVariable     = (*xpReqsBySpellIdArray)[spellIndex];
 
-                            // Get the form!
-                            auto* spellForm = spellIndexToSpellForm[*spellIndex];
-                            if (!spellForm) return RE::BSEventNotifyControl::kContinue;
-                            Log("Spell form: {}", spellForm->GetName());
+                    // Both need to be floats
+                    if (!currentXpVariable.IsFloat()) {
+                        Log("XP variable is not a float!");
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
+                    if (!xpReqVariable.IsFloat()) {
+                        Log("XP req variable is not a float!");
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
 
-                            // Get the original name of this form:
-                            auto originalName = spellIndexToOriginalSpellName[*spellIndex];
-                            if (originalName.empty()) return RE::BSEventNotifyControl::kContinue;
-                            Log("Original name: {}", originalName);
+                    // Get the float values
+                    auto currentXp = currentXpVariable.GetFloat();
+                    auto xpReq     = xpReqVariable.GetFloat();
+                    if (currentXp < 0) {
+                        Log("XP is negative!");
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
+                    if (xpReq <= 0) {
+                        Log("XP req is negative or zero!");
+                        return RE::BSEventNotifyControl::kContinue;
+                    }
 
-                            // Make a new name which is the original name + progress, like
-                            // "<original name> (<progress>%)"
-                            std::string newName = originalName + " (" +
-                                                  std::to_string(static_cast<int>(progress)) + "%)";
+                    // Sweet! Now we can calculate the progress:
+                    auto progress = (currentXp / xpReq) * 100;
 
-                            Log("New name: {}", newName);
+                    // Get it as a percentage, and clamp it to 0-100
+                    auto progressInt = static_cast<std::int32_t>(progress);
+                    if (progressInt < 0) progressInt = 0;
+                    if (progressInt > 100) progressInt = 100;
 
-                            // Update the form name:
-                            spellForm->SetFullName(newName.c_str());
-                            Log("Set new name!");
+                    // Get the form!
+                    auto* spellForm = spellIndexToSpellForm[spellIndex];
+                    if (!spellForm) return RE::BSEventNotifyControl::kContinue;
 
-                        } else {
-                            Log("fXPreq property not found!");
-                        }
+                    // Get the original name of this form:
+                    auto originalName = spellIndexToOriginalSpellName[spellIndex];
+                    if (originalName.empty()) return RE::BSEventNotifyControl::kContinue;
+
+                    // If it's zero, then set the name back to the original name
+                    if (progressInt == 0) {
+                        spellForm->SetFullName(originalName.c_str());
+                    } else {
+                        // Otherwise, set the name to the original name + progress
+                        // Make a new name which is the original name + progress, like
+                        // "<original name> (<progress>%)"
+                        std::string newName =
+                            originalName + " (" + std::to_string(progressInt) + "%)";
+
+                        spellForm->SetFullName(newName.c_str());
                     }
                 }
             }
@@ -197,22 +220,18 @@ SKSEPlugin_OnDataLoaded {
         return;
     }
 
-    // Find all of the spells for the mod which go to the spell index...
-    // For now we look at literally EVERY SPELL FORM ... oof ...
     Log("Searching for Forgotten Magic spells...");
     auto allSpells = RE::TESDataHandler::GetSingleton()->GetFormArray<RE::SpellItem>();
     for (auto* item : allSpells) {
         if (forgottenMagicFile->IsFormInMod(item->GetFormID())) {
             auto spellName = item->fullName;
             if (!spellName.empty()) {
-                Log("Found Forgotten Magic spell: {}", spellName.c_str());
                 auto spellIndex = getSpellIndexFromSpellName(spellName.c_str());
                 if (spellIndex) {
-                    Log("Spell index: {}", *spellIndex);
+                    // Log("Spell index: {}", *spellIndex);
                     spellIndexToSpellForm[*spellIndex]         = item;
                     spellIndexToOriginalSpellName[*spellIndex] = spellName;
-                } else {
-                    Log("Spell index not found for: {}", spellName.c_str());
+                    Log("Spell index: {}, original name: {}", *spellIndex, spellName.c_str());
                 }
             }
         }
